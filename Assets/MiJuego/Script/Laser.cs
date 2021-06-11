6ad027c4769640b81bda8ceeb8f66e4c3d7e9a2d
Nek0VY2 <83292403+Nek0VY2@users.noreply.github.com>
2021-06-11 15:32:47 -0500

    Examn
    todo todito el proyecto

---
+++ Assets/MiJuego/Script/Laser.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float rotationTreshold; //variable del "rango/umbral"
+    public float speed; //variable de la velocidad del movimiento
+    public float maxOffset = 0.2f;
+    [SerializeField]
+    Quaternion maxLeft;
+    [SerializeField]
+    Quaternion maxRight;
+    Coroutine currentCoroutine;
+
+    public UnityEngine.Experimental.Rendering.Universal.Light2D light1;
+    public Color color1;
+    public Color color2;
+    public float tiempo;
+
+    public int damageAmount;
+    private Animator animator;
+    public ParticleSystem particlesHa;
+
+    void Start()
+    {
+        maxLeft.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + rotationTreshold);
+        maxRight.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + rotationTreshold * -1);
+        currentCoroutine = StartCoroutine(MoveLeft());
+
+        light1.color = Color.white;
+        tiempo = Random.Range(0.4f, 1.5f);
+    }
+
+    void Update()
+    {
+        light1.color = Color.Lerp(color1, color2, Mathf.PingPong(Time.time, tiempo));
+    }
+
+    IEnumerator MoveLeft() //IEnumerator implica otro tipo de procesos extra más complejos, requiere que se ejecuten frame by frame, se les conoce como "cortinas".
+    {
+        while (transform.localEulerAngles.z > maxLeft.eulerAngles.z + maxOffset)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, maxLeft, Time.deltaTime * speed); //modificamos el segundo valor, por que es el que nos dira a donde se movera.
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.2f);
+        StopCoroutine(currentCoroutine); //metodo para detener corutinas
+        currentCoroutine = StartCoroutine(MoveRight()); //iniciar la corutina tal.
+    }
+
+    IEnumerator MoveRight()
+    {
+        while (transform.localEulerAngles.z < maxRight.eulerAngles.z - maxOffset)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, maxRight, Time.deltaTime * speed);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.2f);
+        StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(MoveLeft());
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            animator.SetTrigger("HazardActive");
+        }
+    }
+}
---
+++ Assets/MiJuego/Script/PicaFresa.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PicaFresa : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody2D rigidbody;
+
+    [Header("Balance variables")]
+    [SerializeField]
+    public float moveSpeed = 0.1f;
+    [SerializeField]
+    private float jumpForce = 1;
+
+    public int maxHP = 3; // mi entero vale 30 de forma inicial. en este caso mi HP
+    public int currentHP = 3; //si la de arriba es incial, esta vera cuando le pegan y asi
+
+    private bool enPiso;
+    public TextMeshProUGUI vidasUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidasUI.text = currentHP.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && enPiso == true)
+        {
+            rigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);  //agrega una fuerza (addforce) hacia arriba, x se queda en 0f.
+            
+        }
+
+            transform.position = new Vector2(transform.position.x + (moveSpeed/10), transform.position.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) //cuando el trigger entre, es cuando pasa. (cuando entra en colision con el objeto)
+    {
+        if (collision.CompareTag("Hazard"))
+        {
+            if ((currentHP - collision.GetComponent<Laser>().damageAmount) < 0) //comparar con el limite inferior
+            {
+                currentHP = 0;
+                animator.SetTrigger("Dead");
+            }
+
+            else
+            {
+                currentHP -= collision.GetComponent<Laser>().damageAmount; //"-=" hace una resta automatica de lo que tiene mi variable hp , get component es para que traiga a hazard, y ahi aplica su DamageAmount
+                animator.SetTrigger("Damage");
+                vidasUI.text = currentHP.ToString();
+
+            }
+        }
+
+      
+        }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            enPiso = true;
+            animator.SetBool("Jump", false);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            enPiso = false;
+            animator.SetBool("Jump", true);
+        }
+    }
+}
+    
